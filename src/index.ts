342c19a65f61c4e9ae773453c3939eba7acc9d34
Steve Goodrich <hello@stevelgoodrich.com>
2026-04-25 05:43:48 -0400

    feat: add main index.ts entry exports (#11)

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { execa } from './execa.js'
+export { useCassette } from './use-cassette.js'
+export type {
+  Call,
+  Recording,
+  Result,
+  CassetteFile,
+  CassetteSession,
+  Mode,
+  MatcherFn,
+} from './types.js'
